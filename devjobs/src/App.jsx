098d818dc6f1cf18b54bf08f73e3a8ab5b98a07a
Francisco Schmidt <franschmidt2415@gmail.com>
2026-02-19 00:10:15 -0300

    Start React Migration

--- devjobs/src/App.jsx
+++ devjobs/src/App.jsx
@@ -1,33 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="navbar">
+        <h1>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-code">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 8l-4 4l4 4" />
+            <path d="M17 8l4 4l-4 4" />
+            <path d="M14 4l-4 16" />
+          </svg>
+          DevJobs
+        </h1>
+        <nav>
+          <a href="/">Inicio</a>
+          <a href="empleos.html">Empleos</a>
+        </nav>
+        <div>
+          {//<devjobs-avatar service="x" username="midudev" size="32"></devjobs-avatar>
+          }
+        </div>
+      </header>
+
+      <main className="search-main">
+        <section className="search-section">
+          <h1>Encuentra tu próximo trabajo</h1>
+          <p>Explora miles de oportunidades en el sector tecnológico.</p>
+
+          <form role="search">
+            <div className="search-bar">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-search">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                <path d="M21 21l-6 -6" />
+              </svg>
+              <input id="job-search" type="text" placeholder="Buscar trabajos, empresas o habilidades..." />
+            </div>
+
+            <footer className="search-filters">
+              <label for="technology-filter" hidden aria-hidden="true">Tecnología</label>
+              <select id="technology-filter" name="technology">
+                <option value="all">Tecnología</option>
+                <option value="javascript">JavaScript</option>
+                <option value="python">Python</option>
+                <option value="java">Java</option>
+                <option value="kotlin">Kotlin</option>
+                <option value="aws">AWS</option>
+              </select>
+              <label for="location-filter" hidden aria-hidden="true">Ubicación</label>
+              <select id="location-filter" name="location">
+                <option value="all">Ubicación</option>
+                <option value="bsas">Buenos Aires</option>
+                <option value="barcelona">Barcelona</option>
+                <option value="cdmx">Ciudad de México</option>
+                <option value="guadalajara">Guadalajara</option>
+                <option value="madrid">Madrid</option>
+                <option value="valencia">Valencia</option>
+                <option value="bogota">Bogotá</option>
+                <option value="lima">Lima</option>
+                <option value="santiago">Santiago de chile</option>
+                <option value="monterrey">Monterrey</option>
+                <option value="remoto">Remoto</option>
+              </select>
+              <label for="experience-filter" hidden aria-hidden="true">Experiencia</label>
+              <select id="experience-filter" name="experience">
+                <option value="all">Experiencia</option>
+                <option value="junior">Junior</option>
+                <option value="mid">Mid</option>
+                <option value="senior">Senior</option>
+              </select>
+
+            </footer>
+          </form>
+        </section>
+        <section className="search-results">
+          <header>
+            <h2>Resultados de búsqueda</h2>
+          </header>
+          <footer className="results-list">
+          </footer>
+          <p className="results-number"></p>
+        </section>
+        <nav className="pagination">
+
+        </nav>
+      </main>
+      <footer className="global-footer">
+        <small>&copy; 2026 DevJobs. Todos los derechos reservados.</small>
+      </footer>
+
     </>
   )
 }
